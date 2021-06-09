3cfcf132098efffbb3f2ae0e9934b90317b44305
khanhevans97 <35840926+khanhevans97@users.noreply.github.com>
2021-06-09 16:56:18 +0700

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ const CONFIG = {
     Mà sao em cứ đi nhầm đường
     lạc vào tim anh.
     Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'Ahihi My chóa điên giỏi nhấn zô đây',
+    btnIntro: 'My chóa điên giỏi nhấn zô đây',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
     btnYes: 'Vẫn cứ là thích anh <33',
