0f46fbdf18877eee3c002d67db3cd85943f0bf0b
khanhevans97 <35840926+khanhevans97@users.noreply.github.com>
2021-06-09 09:22:09 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 const CONFIG = {
     introTitle: 'Bấy bê à!',
-    introDesc: `Trái đất vốn lạ thường
+    introDesc: `Trái đất vốn lạ thường.
     Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
+    lạc vào tim anh.
     Đằng sau chữ yêu đây là thương`,
     btnIntro: 'hihi',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
@@ -15,5 +15,5 @@ const CONFIG = {
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
     messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: 'https://www.facebook.com/khanhevan/' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
